26eccf76eb989f0fbec2d5ce53cbeff59b80bbd4
RafiaChy <rafiachowdhury24@gmail.com>
2020-12-12 11:37:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/beautysplash/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.beautysplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout myLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myLayout = findViewById(R.id.makeuplayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+    }
+}
